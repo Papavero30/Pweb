79fcf8af1d30d784078e411bf64c827adec164bb
Muhammad Nabil Fadhil <nabilfadhil30@gmail.com>
2023-10-18 09:52:11 +0700

    hehe
    salah

--- ETS_Pweb/main.js
+++ ETS_Pweb/main.js
@@ -1,34 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const apiUrl = "http://it-its.id/api/movies";
-  const movieContainer = document.querySelector(".row");
+    const apiUrl = "http://it-its.id/api/movies";
+    const movieContainer = document.querySelector(".row");
 
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((movieData) => {
-        const ratings = movieData.Ratings.map((rating) => `${rating.Source}: ${rating.Value}`).join("<br>");
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach((movieData) => {
+                const ratings = movieData.Ratings.map((rating) => `${rating.Source}: ${rating.Value}`).join("<br>");
 
-        const movieElement = document.createElement("div");
-        movieElement.className = "col-6";
-        movieElement.innerHTML = `
-          <div class="mt-4 p-5 bg-primary text-white rounded">
-              <h2>Title: ${movieData.Title}</h2>
-              <p>Director: ${movieData.Director}</p>
-              <p>Genre: ${movieData.Genre}</p>
-              <p>Released: ${movieData.Released}</p>
-              <p>IMDb Rating: ${movieData.imdbRating}</p>
-              <p>Metascore: ${movieData.Metascore}</p>
+                const movieElement = document.createElement("div");
+                movieElement.className = "col-6";
+                movieElement.innerHTML = `
+          <div class="mt-4 p-5 jumbotron-custom">
+              <h2>${movieData.Title}</h2>
               <p>${movieData.Plot}</p>
-              <h2>Ratings</h2>
-              <p>${ratings}</p>
           </div>
         `;
-        movieContainer.appendChild(movieElement);
-      });
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+                movieContainer.appendChild(movieElement);
+            });
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
 });
 
 
