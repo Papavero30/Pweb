1769985179937134ce4fadebc349248bf990125d
Muhammad Nabil Fadhil <nabilfadhil30@gmail.com>
2023-10-18 10:02:46 +0700

    sda
    dsa

--- ETS_Pweb/main.js
+++ ETS_Pweb/main.js
@@ -7,13 +7,12 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((data) => {
             data.forEach((movieData) => {
                 const ratings = movieData.Ratings.map((rating) => `${rating.Source}: ${rating.Value}`).join("<br>");
-
                 const movieElement = document.createElement("div");
                 movieElement.className = "col-6";
                 movieElement.innerHTML = `
           <div class="mt-4 p-5 jumbotron-custom">
-              <h2>${movieData.Title}</h2>
               <p>${movieData.Plot}</p>
+
           </div>
         `;
                 movieContainer.appendChild(movieElement);
